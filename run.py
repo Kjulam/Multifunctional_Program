e1ac4a13c300c223f60daf36e6aba0f74b9749d8
Kjulam <kjulamotlkcm@outlook.com>
2025-04-17 21:05:25 +0800

    新增求平均数功能。修复需要__main__()函数才能运行的致命bug。

--- run.py
+++ run.py
@@ -2,21 +2,25 @@
 import sys
 from funcs.__main__.main import main
 
-if len(sys.argv) >= 2:
-    from funcs.__main__.print_help import print_help
-    if "--help" in sys.argv:
-        print_help()
+def run():
+    if len(sys.argv) >= 2:
+        from funcs.__main__.print_help import print_help
+        if "--help" in sys.argv:
+            print_help()
 
-    elif "--create-new-module" in sys.argv:
-        from funcs.__main__.create_new_module import create_new_module
-        create_new_module(sys.argv)
+        elif "--create-new-module" in sys.argv:
+            from funcs.__main__.create_new_module import create_new_module
+            create_new_module(sys.argv)
 
-    else:
-        print("错误：你不会加参数就别加了。")
-        print_help()
-        sys.exit(1)
+        else:
+            print("错误：你不会加参数就别加了。")
+            print_help()
+            sys.exit(1)
 
-    sys.exit(0)
+        sys.exit(0)
 
-print(f"程序已退出，退出代码：{main("funcs")}。")
+    print(f"程序已退出，退出代码：{main("funcs")}。")
+
+if __name__ == "__main__":
+    run()
 
